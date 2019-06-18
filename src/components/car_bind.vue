6a51fb532ed0f29a939261f65bcd2cc4358e5c06
liwanning <liwanning@promni.cn>
2019-06-18 12:23:48 +0800

    refactor:优化车牌键盘代码。

--- src/components/car_bind.vue
+++ src/components/car_bind.vue
@@ -1,182 +1,177 @@
 <template lang="pug">
-    .car_bind
-        sticky
-            tab(:line-width="1")
-                tab-item(selected,@on-item-click="changeCar") 机动车车牌
-                tab-item(@on-item-click="changeCar") 新能源车牌
-        h2.enter_number 输入车牌号
-        span.remark
-        .code
-            //clickBtn是为了确定黄色底标在哪个位置出现及让键盘出现
-            button.index(:new="show===true",@click="clickBtn(1)" ) {{one}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(2)") {{two}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(3)") {{three}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(4)") {{four}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(5)") {{five}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(6)") {{six}}
-            span.line
-            button.index(:new="show===true",@click="clickBtn(7)") {{seven}}
-            span(v-if="show===true").line
-            button(v-if="show===true").index(:new="show===true",@click="clickBtn(8)") {{eight}}
-        x-button.add-car 添加车牌
-        .keyboard(v-for="item in list")
-            .row
-                button.btn(v-for="item in list.one",@click="changeKeyboard(item)",:disabled="disable===true") {{item}}
-            .row
-                button.btn(v-for="item in list.two",@click="changeKeyboard(item)") {{item}}
-            .row
-                button.btn(v-for="item in list.three",@click="changeKeyboard(item)") {{item}}
-            .row
-                button.btn(v-for="item in list.four",@click="changeKeyboard(item)") {{item}}
-
-
+    div
+        .car_bind
+            .code
+                button.spot
+                    div
+                        //index1从0到7
+                button.index(v-for="(item,index1) in carList", @click="clickBtn(index1,item)", :class="index1===index?'greencolor':''") {{item}}
+            h2.enter_number 请输入车牌号
+            button.add-car(:disabled="isClick",@click="bindCar",:class="!isClick?'':'grey'") 绑定车牌
+            .keyboard(v-for="item in list")
+                .row
+                    button.btn(v-for="item in list.one",@click="changeKeyboard(item)",) {{item}}
+                .row
+                    button.btn(v-for="item in list.two",@click="changeKeyboard(item)") {{item}}
+                .row
+                    button.btn(v-for="item in list.three",@click="changeKeyboard(item)") {{item}}
+                .row
+                    button.switch(@click="change")
+                        img.switch-img(:src="imgSrc")
+                    button.btn(v-for="item in list.four",@click="changeKeyboard(item)") {{item}}
+                    button.delete.btn(@click="deleteCode")
+                        img.deleter-arrow(src="../assets/img/arrow.png")
 </template>
 <script>
-    import { Tab, TabItem, Sticky,XButton  } from 'vux'
-    import $ from "jquery"
-    export default {
-        data() {
-            return {
-                title: '我要停车',
-                show:false,
-                key:false,
-                list:'',
-                //车牌号第二位时键盘只能输入字母
-                disable:false,
-                //车牌号每格对应的字
-                one:'',
-                two:'',
-                three:'',
-                four:'',
-                five:'',
-                six:'',
-                seven:'',
-                eight:'',
-                //记录当前车牌号黄色底标的位置
-                index:'',
-                multiplier:1/14,
-                rowOne:{
-                    one:['京','津','冀','鲁','晋','蒙','辽','吉','黑','沪'],
-                    two:['苏','浙','皖','闽','赣','豫','鄂','湘','粤','桂'],
-                    three:['渝','川','贵','云','藏','陕','甘','青'],
-                    four:['琼','新','港','澳','台','宁'],
-                },
-                rowTwo:{
-                    one:['1','2','3','4','5','6','7','8','9','0'],
-                    two:['Q','W','E','R','T','Y','U','I','O','P'],
-                    three:['A','S','D','F','G','H','J','K','L'],
-                    four:['A','S','D','F','G','H','J','K','L']
-                },
+  import { Tab, TabItem, Sticky, XButton } from "vux";
+  import headerTitle from "@/components/header_title";
 
-            }
+  export default {
+    data() {
+      return {
+        title: "更改车牌",
+        item: "",
+        list: "",
+        isNumber: false,
+        //控制绑定车牌是否可以点击
+        isClick: true,
+        //车牌号每格对应的字
+        carNum: "",
+        //记录当前绿色背景的位置
+        index: "",
+        imgSrc: "",
+        carList: ["", "", "", "", "", "", "", ""],
+        rowOne: {
+          one: ["京", "津", "冀", "鲁", "晋", "蒙", "辽", "吉", "黑", "沪"],
+          two: ["苏", "浙", "皖", "闽", "赣", "豫", "鄂", "湘", "粤", "桂"],
+          three: ["琼", "渝", "川", "贵", "云", "藏", "陕", "甘", "青", "宁"],
+          four: ["新", "港", "澳", "台", "使", "领", "警"]
         },
-        components: {
-            Tab,
-            TabItem,
-            Sticky,
-            XButton
-        },
-        mounted: function () {
-            this.$nextTick(function () {
-                this.onceInit(this.init);
-            });
-        },
-        methods: {
-            onceInit: function (callback) {
-                callback();
-            },
-            init: function () {
-
-            },
-            changeCar:function(index){
-                if(index===0){
-                    this.multiplier=1/14;
-                    this.show=false;
-                }
-                if(index===1){
-                    this.multiplier=1/16;
-                    this.show=true;
-                }
-                if(this.index){
-                    let meter=(2*this.index-1)*this.multiplier*document.body.clientWidth-7;
-                    $('span.remark.yellow').css('left',meter);
-                }
-            },
-            clickBtn:function(number){
-                this.index=number;
-                $('span.remark').addClass('yellow');
-                let meter=(2*number-1)*this.multiplier*document.body.clientWidth-7;
-                $('span.remark.yellow').css('left',meter);
-                this.key=!this.key;
-                if(number===1){
-                    this.list=this.rowOne;
-                }else{
-                    this.list=this.rowTwo;
-                }
-                if(number==2){
-                    this.disable=true;
-                }else{
-                    this.disable=false;
-                }
-            },
-            changeKeyboard:function(item){
-                this.list=this.rowTwo;
-                switch(this.index){
-                    case 1:
-                        this.one=item;
-                        let meter1=(2*2-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter1);
-                        this.index+=1;
-                        this.disable=true;
-                        break;
-                    case 2:
-                        this.two=item;
-                        let meter2=(2*3-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter2);
-                        this.index+=1;
-                        this.disable=false;
-                        break;
-                    case 3:
-                        this.three=item;
-                        let meter3=(2*4-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter3);
-                        this.index+=1;
-                        break;
-                    case 4:
-                        this.four=item;
-                        let meter4=(2*5-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter4);
-                        this.index+=1;
-                        break;
-                    case 5:
-                        this.five=item;
-                        let meter5=(2*6-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter5);
-                        this.index+=1;
-                        break;
-                    case 6:
-                        this.six=item;
-                        let meter6=(2*7-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter6);
-                        this.index+=1;
-                        break;
-                    case 7:
-                        this.seven=item;
-                        this.index+=1;
-                        break;
-                    case 8:
-                        this.eight=item;
-                        let meter8=(2*8-1)*this.multiplier*document.body.clientWidth-7;
-                        $('span.remark.yellow').css('left',meter8);
-                        this.index+=1;
-                        break;
-                }
-            }
+        rowTwo: {
+          one: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+          two: ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+          three: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+          four: ["Z", "X", "C", "V", "B", "N", "M"]
         }
+      };
+    },
+    watch: {
+      //通过光标的移动判断当前输入的车牌号位数，根据车牌号位数判断是否可以点击绑定车牌
+      index: function() {
+        var vm = this;
+        if (vm.carList.indexOf("") == 7 || vm.carList.indexOf("") == -1) {
+          vm.isClick = false;
+        } else {
+          vm.isClick = true;
+        }
+        if (vm.carList[0] != "警" && vm.carList[0] != "使" && vm.carList[0] != "领" && Number(vm.carList[1]) && vm.index == 1) {
+          vm.carList[1] = "";
+        }
+
+      }
+    },
+    components: {
+      Tab,
+      TabItem,
+      Sticky,
+      XButton,
+      headerTitle
+    },
+    mounted: function() {
+      var vm = this;
+      this.$nextTick(function() {
+        this.onceInit(this.init);
+      });
+    },
+    methods: {
+      onceInit: function(callback) {
+        callback();
+      },
+      change() {
+        var vm = this;
+        vm.list = vm.isNumber ? vm.rowOne : vm.rowTwo;
+        vm.isNumber = !vm.isNumber;
+        vm.imgSrc = vm.isNumber ? require("../assets/img/administrative.png") : require("../assets/img/number.png");
+      },
+      //绑定车牌业务
+      bindCar: function() {
+        var vm = this;
+        vm.carNum = vm.carList.join("");
+        var params = {
+          carNum: vm.carNum
+        };
+
+      },
+      init: function() {
+        var vm = this;
+        vm.carList = ["", "", "", "", "", "", "", ""];
+        vm.imgSrc = require("../assets/img/number.png");
+      },
+      //点击上方请输入车牌号触发的事件
+      clickBtn: function(number, code) {
+        var vm = this;
+        vm.index = number;
+        vm.disable = number === 1;
+        console.log(vm.carList[0]);
+        //控制键盘切换
+        if (number === 0) {
+          vm.list = vm.rowOne;
+          vm.isNumber = false;
+          vm.imgSrc = require("../assets/img/number.png");
+        } else {
+          vm.list = vm.rowTwo;
+          vm.isNumber = true;
+          vm.imgSrc = require("../assets/img/administrative.png");
+        }
+        //控制键盘第一排数字是否可以点击
+        // if (number == 1) {
+        //   vm.disable = true;
+        // } else {
+        //   vm.list = vm.rowTwo;
+        //   if(number===1&&(vm.carList[0]=='警'||vm.carList[0]=='使'||vm.carList[0]=='领')){
+        //   }
+        // }
+        if (vm.index === 0 && code != "领" && code != "使" && code != "警" && typeof vm.carList[1] == Number) {
+          vm.carList[1] = "";
+        }
+      },
+      //点击键盘时触发
+      changeKeyboard: function(item) {
+        var vm = this;
+        vm.item = item;
+        //从第一个键盘切换到第二个键盘
+        vm.list = vm.rowTwo;
+        vm.isNumber = true;
+        vm.imgSrc = require("../assets/img/administrative.png");
+        //将点击触发的文字或者字母信息放进车牌list里，更新上方车牌号
+        vm.$set(vm.carList, vm.index, item);
+        if (vm.index >= 7) {
+          return;
+        } else {
+          vm.index++;
+        }
+      },
+      //删除车牌时候触发
+      deleteCode: function() {
+        var vm = this;
+        if (!vm.carList[vm.index] && vm.index !== 0) {
+          vm.index = Number(vm.index) - 1;
+        }
+        vm.$set(vm.carList, vm.index, "");
+        if (vm.carList.indexOf("") == 7 || vm.carList.indexOf("") == -1) {
+          vm.isClick = false;
+        } else {
+          vm.isClick = true;
+        }
+        if (vm.index === 0) {
+          vm.list = vm.rowOne;
+          vm.isNumber=false;
+          vm.imgSrc = require("../assets/img/number.png");
+        }
+      }
     }
+  };
 </script>
+<style scoped>
+    @import '../assets/css/car.css';
+</style>
---
+++ src/components/header_title.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+    div.header-title(@click="back")
+        i.back-icon.fa.fa-chevron-left
+        span {{title}}
+</template>
+<style scoped>
+    .header-title {
+        width: 100%;
+        height: 3rem;
+        line-height: 3rem;
+        font-size: 1rem;
+        position: fixed;
+        top: 0;
+        padding-left: 1.5rem;
+        vertical-align: middle;
+        background: #fff;
+        z-index: 10000;
+    }
+
+    .header-title i.back-icon {
+        font-size: 1.2rem;
+        color: #757575;
+        margin-right: .5rem;
+        vertical-align: middle;
+    }
+
+    .header-title span {
+        vertical-align: middle;
+    }
+</style>
+<script>
+  export default {
+    props: ['title'],
+    methods: {
+      back: function () {
+        var vm = this;
+        //为了防止一直在pay页面
+      }
+    }
+  }
+</script>
